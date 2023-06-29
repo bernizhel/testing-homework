6010544ca653098735fd63e5870b3f1ff3518a3d
bernizhel <detrop.1995@gmail.com>
2023-06-29 14:39:37 +0300

    test(hermione/desktop): update paths

--- .hermione.conf.js
+++ .hermione.conf.js
@@ -1,7 +1,7 @@
 module.exports = {
     sets: {
         desktop: {
-            files: 'test/hermione/desktop',
+            files: 'test/hermione/desktop/**.hermione.js',
         },
     },
     browsers: {
