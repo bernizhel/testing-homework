bb4baf09d0f3478367e6c6e760a62de097217f8d
bernizhel <detrop.1995@gmail.com>
2023-06-29 14:18:08 +0300

    test(hermione): add constants

---
+++ test/hermione/constant.js
@@ -0,0 +1,9 @@
+const PROTOCOL = 'http';
+const DOMAIN = 'localhost';
+const PORT = 3000;
+const PATHNAME = '/hw/store';
+const SUT_URI = `${PROTOCOL}://${DOMAIN}:${PORT}${PATHNAME}`;
+
+module.exports = {
+    SUT_URI,
+};
