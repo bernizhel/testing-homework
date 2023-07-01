d0ea1a11f7530a0f917e950a2eb68b8f980fc218
bernizhel <detrop.1995@gmail.com>
2023-07-01 18:32:18 +0300

    test(hermione): configure hermione

--- .hermione.conf.js
+++ .hermione.conf.js
@@ -1,19 +1,59 @@
-module.exports = {
-    sets: {
-        desktop: {
-            files: 'test/hermione/desktop/**.hermione.js',
-        },
+const os = require('node:os');
+
+// will be changed by the pageobject
+const RESOLUTIONS = [
+    {
+        width: 1920,
+        height: 1080,
+        isMobile: false,
     },
+    {
+        width: 1280,
+        height: 720,
+        isMobile: false,
+    },
+    {
+        width: 1024,
+        height: 1080,
+        isMobile: false,
+    },
+    {
+        width: 720,
+        height: 480,
+        isMobile: false,
+    },
+    {
+        width: 480,
+        height: 720,
+        isMobile: true,
+    },
+    {
+        width: 320,
+        height: 480,
+        isMobile: true,
+    },
+];
+
+const SUT_URI = 'http://localhost:3000/hw/store/';
+const DEFAULT_TIMEOUT = 5000;
+const DEFAULT_SELECTOR = '.Application';
+
+module.exports = {
     browsers: {
-        chrome: {
-            automationProtocol: 'devtools',
-            desiredCapabilities: {
-                browserName: 'chrome',
-            },
-            windowSize: {
-                width: 1920,
-                height: 1080,
-            },
+        ...getConfigurationObject(getBrowserOptions),
+    },
+    sets: {
+        ...getConfigurationObject(getTestSet),
+    },
+    system: {
+        fileExtensions: ['.js'],
+        parallelLimit: os.cpus().length,
+        // use any number of workers your machine allows
+        workers: 1,
+        ctx: {
+            sutUri: SUT_URI,
+            defaultTimeout: DEFAULT_TIMEOUT,
+            defaultSelector: DEFAULT_SELECTOR,
         },
     },
     plugins: {
@@ -22,3 +62,54 @@ module.exports = {
         },
     },
 };
+
+function getConfigurationObject(fn, array = RESOLUTIONS) {
+    return array.map(fn).reduce((obj, next) => Object.assign(obj, next), {});
+}
+
+function getTestSet({ width }) {
+    return {
+        [width]: {
+            files: [
+                'test/hermione/common/**/*.hermione.js',
+                `test/hermione/${width}/**/*.hermione.js`,
+            ],
+            browsers: [`chrome:${width}`],
+        },
+    };
+}
+
+function getBrowserOptions({ height, width, isMobile }) {
+    return {
+        [`chrome:${width}`]: {
+            automationProtocol: 'devtools',
+            desiredCapabilities: {
+                browserName: 'chrome',
+                version: '114',
+            },
+            windowSize: {
+                width,
+                height,
+            },
+            sessionEnvFlags: {
+                isChrome: true,
+                isMobile,
+            },
+            httpTimeout: DEFAULT_TIMEOUT,
+            retry: 2,
+            resetCursor: true,
+            calibrate: true,
+            compositeImage: false,
+            screenshotDelay: 100,
+            screenshotsDir: 'hermione/screens',
+            screenshotMode: 'fullpage',
+            assertViewOpts: {
+                allowViewportOverflow: true,
+                captureElementFromTop: true,
+                selectorToScroll: DEFAULT_SELECTOR,
+            },
+            // baseUrl: 'http://localhost:3000/hw/store/',
+            // transformRequest: null,
+        },
+    };
+}
