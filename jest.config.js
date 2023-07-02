90485db333f99ea3d631832dca7f1b468dedebfd
bernizhel <detrop.1995@gmail.com>
2023-07-02 22:32:08 +0300

    test(jest): configure jest and babel

--- jest.config.js
+++ jest.config.js
@@ -1,4 +1,16 @@
 module.exports = {
-    preset: 'ts-jest',
+    verbose: true,
     testEnvironment: 'jsdom',
+    preset: 'ts-jest',
+    moduleFileExtensions: ['js', 'jsx', 'ts', 'tsx'],
+    moduleDirectories: ['node_modules', 'src', 'test'],
+    transform: {
+        '^.+\\.(ts|tsx)?$': 'ts-jest',
+        '^.+\\.(js|jsx)$': 'babel-jest',
+    },
+    globals: {
+        'ts-jest': {
+            isolatedModules: true,
+        },
+    },
 };
